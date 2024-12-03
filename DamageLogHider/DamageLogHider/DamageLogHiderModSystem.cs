a053c7f65c3f20fdee96a570607a9104f4c7276c
Siim Raudsepp <siimraudsepp@msn.com>
2024-12-03 23:50:45 +0200

    Add mod files

---
+++ DamageLogHider/DamageLogHider/DamageLogHiderModSystem.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Server;
+using Vintagestory.API.Config;
+using Vintagestory.API.Common;
+using Vintagestory.Client.NoObf;
+
+namespace DamageLogHider;
+
+public class DamageLogHiderModSystem : ModSystem
+{
+    public static ICoreServerAPI serverApi;
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        serverApi = api;
+        var harmony = new Harmony(Mod.Info.ModID);
+
+        var original = AccessTools.Method(typeof(EntityPlayer), nameof(EntityPlayer.OnHurt));
+        var patched = AccessTools.Method(typeof(OnHurtPatch), nameof(OnHurtPatch.Prefix));
+        
+        harmony.Patch(original, prefix: new HarmonyMethod(patched));
+    }
+
+}
---
+++ DamageLogHider/DamageLogHider/OnHurtPatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace DamageLogHider;
+
+public class OnHurtPatch
+{
+    [HarmonyPatch(typeof(EntityPlayer), nameof(EntityPlayer.OnHurt))]
+    public static bool Prefix(EntityPlayer __instance, DamageSource damageSource, float damage)
+    {
+        if (damageSource?.Source == EnumDamageSource.Player)
+        {
+            var world = __instance.World;
+            var heal = damageSource.Type == EnumDamageType.Heal;
+            
+            
+            var msg = Lang.Get(heal ? "damagelog-heal-byplayer" : "damagelog-damage-byplayer", damage, string.Empty);
+
+            (world.PlayerByUid(__instance.PlayerUID) as IServerPlayer).SendMessage(GlobalConstants.DamageLogChatGroup, msg, EnumChatType.Notification);
+
+            return false;
+        }
+
+        return true;
+    }
+}
